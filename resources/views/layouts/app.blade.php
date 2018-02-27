2dd0967d2b4d374745e5a5a24ea36c9dc04e2745
Artiaga <alesev1993@gmail.com>
2018-02-27 20:25:42 +0100

    vista arreglada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,10 +23,6 @@
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{'Gestor Salas' }}
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
             @auth
             <a class="navbar-brand" href="{{ url('/') }}/crear">Crear Salas</a>
